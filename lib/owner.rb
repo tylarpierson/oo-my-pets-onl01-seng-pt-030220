604661ae03be911a783902f196ba1b41ba39acd9
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-07 15:32:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
-class Owner
-  # code goes here
-end
+class Owner 
+  attr_reader :name, :species
+  attr_accessor :pets  
+  
+  @@all = []
+   
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.size 
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end 
+  
+  def cats 
+    @pets[:cats] 
+  end 
+end 
